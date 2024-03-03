d21705f93f4e5046848b38947bf7f8723393ac2d
XDigital123 <khantofik12356@gmail.com>
2024-03-03 18:40:20 +0530

    square print

---
+++ 9) print the square of each value using forEach loop.js
@@ -0,0 +1,14 @@
+//print the sqare of each value using for each loop 
+// 1) way 
+const nums = [ 1,2,3,4,5];
+nums.forEach(function(num) {
+    console.log(num*num);
+})
+
+
+
+// 2) way
+const values = [ 1,2,3,4,5];
+values.forEach((num) => {
+    console.log(num*num);
+})
